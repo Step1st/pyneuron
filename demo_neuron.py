740f5ee1c2a7f22df5229f3f2e9dfc82f91a7752
Step1st <Step1st@users.noreply.github.com>
2022-09-19 14:32:56 +0300

    created init_args function

--- demo_neuron.py
+++ demo_neuron.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from neuron import *
-from init_args import args
+from init_args import init_args
 
 # Load data from csv
 data = pd.read_csv("demo_data.csv")
@@ -9,14 +9,17 @@ data = data.drop("t", axis=1)
 data = data.values.tolist()
 weight_count = len(data[0])
 
+# Process command line arguments
+args = init_args()
 neuron_count = args.neuron_count
 activation_function = sigmoid if args.activation_function == 'sigmoid' else binary_step
 threshold = args.threshold
 weight_generator = RandomWeightGenerator(args.range[0], args.range[1], args.precision)
 
 neuron = Neuron(weight_count, activation_function, weight_generator)
-total_neuron_count = 0
 
+# Generate neurons
+total_neuron_count = 0
 for i in range(neuron_count):
     while True:
         total_neuron_count += 1
@@ -26,9 +29,7 @@ for i in range(neuron_count):
         processed_outputs = round_outputs(raw_outputs, threshold) 
 
         if processed_outputs == labels:
-            print(f"{i+1}. {neuron}")
-            print(f"Output: {processed_outputs}")
-            print(f"Raw output: {raw_outputs}")
+            print(f"{i+1}. {neuron}, outputs: {raw_outputs}")
             break
 
 print(f"Went through {total_neuron_count} neurons")
--- init_args.py
+++ init_args.py
@@ -1,10 +1,15 @@
 import argparse
 
-parser = argparse.ArgumentParser(description = 'Neuron demo')
-parser.add_argument('-n', type=int, default = 1, help = 'number of neurons to create', dest = 'neuron_count')
-parser.add_argument('-f', type=str, default ='sigmoid', help = 'activation function to use', dest = 'activation_function', choices = ['sigmoid', 'binarystep'])
-parser.add_argument('-t', type=float, default = 0.5, help = 'threshold for classification', dest = 'threshold')
-parser.add_argument('-r', type=float, default = [-1, 1], help = 'range for random weights', dest = 'range', nargs = 2, metavar = ('MIN', 'MAX'))
-parser.add_argument('-p', type=int, default = None, help = 'precision for random weights', dest = 'precision')
+def add_arguments(parser):
+    parser.add_argument('-n', type=int, default = 1, help = 'number of neurons to create', dest = 'neuron_count')
+    parser.add_argument('-f', type=str, default ='sigmoid', help = 'activation function to use', dest = 'activation_function', choices = ['sigmoid', 'binarystep'])
+    parser.add_argument('-t', type=float, default = 0.5, help = 'threshold for classification', dest = 'threshold')
+    parser.add_argument('-r', type=float, default = [-1, 1], help = 'range for random weights', dest = 'range', nargs = 2, metavar = ('MIN', 'MAX'))
+    parser.add_argument('-p', type=int, default = None, help = 'precision for random weights', dest = 'precision')
+    return parser
 
-args = parser.parse_args()
+def init_args():
+    parser = argparse.ArgumentParser(description = 'Neuron demo')
+    parser = add_arguments(parser)
+    args = parser.parse_args()
+    return args
